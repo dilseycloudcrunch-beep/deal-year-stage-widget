95e052d7c53eb2ca7923f2af6c3059c9c3d84dda
dilseycloudcrunch-beep <dilsey.cloudcrunch@gmail.com>
2026-08-18 00:55:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,6 @@ async function fetchDealsForYear(year) {
     allDeals = yearDeals;
 
     if (allDeals.length > 0) {
-        console.log("Sample deal object:", JSON.stringify(allDeals[0], null, 2));
       populateOwnerDropdown();
       renderStages();
     } else {
